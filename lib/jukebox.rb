30fa100ecc99cf902818b5f1cf88988c1aca2538
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 20:34:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,4 @@
-    songs = [
+songs = [
       "Phoenix - 1901",
       "Tokyo Police Club - Wait Up",
       "Sufjan Stevens - Too Much",
@@ -8,12 +8,16 @@
       "Phoenix - Consolation Prizes",
       "Harry Chapin - Cats in the Cradle",
       "Amos Lee - Keep It Loose, Keep It Tight"
-    ]
+]
     
-    def help 
-      puts "I accept the following commands:"
-      puts "- help : displays this help message"
-      puts "- list : displays a list of songs you can play"
-      puts "- play : lets you choose a song to play"
-      puts ""
-    end
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+    
+def list songs
+  
+end
